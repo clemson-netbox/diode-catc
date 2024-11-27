b8f23aaa4054189dae8674f969b9b3a01aa0e4d4
Eric Hester <eric.hester@umbrella.tech>
2024-11-26 19:10:42 -0500

    debug

--- catc_fetcher.py
+++ catc_fetcher.py
@@ -49,6 +49,7 @@ def get_sites_with_devices(client):
 def get_device_details(client):
 
     devices = get_paginated_objects(client.devices.get_device_list, "devices")
+    print(f"{devices}")
     return {device.response.serialNumber: device.response for device in devices}
 
 def get_interfaces(client, device_id):
