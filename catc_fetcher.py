b56df4ecf9ba034692ea7e503828554ad07d46c4
Eric Hester <eric.hester@umbrella.tech>
2024-11-26 21:12:56 -0500

    added error checking

--- catc_fetcher.py
+++ catc_fetcher.py
@@ -18,30 +18,47 @@ def get_device_data(client):
         logging.info(f"Retrieved {items} devices")
     logging.info('Collected complete device list from Cisco Catalyst Center')
 
+    response = client.sites.get_site_count()
+    site_count = response['response']
+    logging.info('Number of devices managed by Cisco Catalyst Center: ' + str(site_count))
+
+    offset = 1
+    limit = 500
+    site_list = []
+    items=0
+    while offset <= site_count:
+        response = client.sites.get_site(offset=offset)
+        offset += limit
+        site_list.extend(response['response'])
+        items += limit
+        logging.info(f"Retrieved {items} sites")
+    logging.info('Collected complete site list from Cisco Catalyst Center')
+
     device_inventory = []
         
     items=0
     for device in device_list:
         interfaces = []
-        
-        # response = client.devices.get_device_detail(identifier='uuid', search_by=device['id'])
-        # site = response['response']['location']
-        # device.update({'site': site})
-        # logging.info(f"Found device #{items}/{str(device_count)}: {device['hostname']} in {site}")
+    
         logging.info(f"Found device #{items}/{str(device_count)}: {device['hostname']}")
         
-        if not 'Unified AP' in device.family:
-            try:
-                response = client.devices.get_interface_info_by_id(device_id=device['id'])
-            except:
-                logging.error(f"No interfaces found for device {device['hostname']}")
+        try:
+            if not 'Unified AP' in device.family:
+                try:
+                    response = client.devices.get_interface_info_by_id(device_id=device['id'])
+                except:
+                    logging.info(f"No interfaces found for device {device['hostname']}")
+                    device_inventory.append(device)
+                    continue
+            else:
+                logging.info(f"No interfaces found for device {device['hostname']}")
                 device_inventory.append(device)
                 continue
-        else:
-            logging.error(f"No interfaces found for device {device['hostname']}")
-            device_inventory.append(device)
+
+        except Exception as e:
+            logging.error(f"An error occurred collecting device data: {e}")
             continue
-            
+        
         interfaces.extend(response['response'])  
         logging.info(f"Found {len(interfaces)} interfaces for {device['hostname']}")
         device.interfaces=interfaces
