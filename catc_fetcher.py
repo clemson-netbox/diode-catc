1a571825a46f192c78dca6162757387a1471c8db
Eric Hester <eric.hester@umbrella.tech>
2024-11-26 22:07:00 -0500

    .

--- catc_fetcher.py
+++ catc_fetcher.py
@@ -34,7 +34,7 @@ def get_device_data(client):
         logging.info(f"Retrieved {items} sites")
     logging.info('Collected complete site list from Cisco Catalyst Center')
 
-    site_sn=[]
+    site_sn={}
     logging.info('Retrieving device locations')
     for site in site_list:
         if site['siteNameHierarchy'] == 'Global': continue
