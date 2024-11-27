9dc40a686e209b962b03917ebc7280beaa4e5667
Eric Hester <eric.hester@umbrella.tech>
2024-11-26 20:54:19 -0500

    .

--- catc_fetcher.py
+++ catc_fetcher.py
@@ -18,28 +18,26 @@ def get_device_data(client):
         device_list.extend(response['response'])
         items += limit
         logging.info(f"Retrieved {items} devices")
-    logging.info('Collected the device list from Cisco Catalyst Center')
+    logging.info('Collected complete device list from Cisco Catalyst Center')
 
     device_inventory = []
     for device in device_list:
 
-        # get the device site hierarchy
         response = client.devices.get_device_detail(identifier='uuid', search_by=device['id'])
         site = response['response']['location']
         device.update({'site': site})
-        logging.info(f"Collected site {site} for {device['hostname']}")
+        logging.info(f"Found {device['hostname']} in {site}")
 
         try:
             response = client.devices.get_interface_info_by_id(device_id=device['id'])
         except:
-            logging.error("No interfaces found for device {device['hostname']}")
+            logging.error(f"No interfaces found for device {device['hostname']}")
             
         interfaces.extend(response['response'])  
         for interface in interfaces:
-            logging.info(f"Collected interface {interface.portName} for {device['hostname']}")
+            logging.info(f"Found interface {interface.portName} for {device['hostname']}")
         device.interfaces=interfaces
         
         device_inventory.append(device)
         
-    logging.info('Collected the device inventory from Cisco DNA Center')
     return device_inventory
